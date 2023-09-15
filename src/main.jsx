04faf70198d9ad28b89857e17cc08415c815b3b9
blanq31 <obiwulugodswill@gmail.com>
2023-09-15 02:39:26 +0100

    CRUD Operations done
    CRUD operations are finally done.. RTK is so much simpler than react-redux/@reduxjs/toolkit or maybe I haven't gotten used to them

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react/';
+import { apiSlice } from './features/api/apiSlice.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ApiProvider api={apiSlice}>
+      <App />
+    </ApiProvider>
   </React.StrictMode>,
 )
